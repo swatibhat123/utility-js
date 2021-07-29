943315464dd3ad263386de33e8ddd8ce94d8c899
Swati Bhat <swati.bhat@thoughtworks.com>
2021-07-29 12:56:00 +0530

    Added actual implementation for tail function

--- src/tail.js
+++ src/tail.js
@@ -1,9 +1,13 @@
 const tail = (inputArray)=>{
-
-    if(inputArray.length === 0)
+    
+    const sizeOfEmptyArray = 0;
+    if(inputArray.length === sizeOfEmptyArray)
       return inputArray;
     
-    
+    const startIndex = 1;
+    const tailArray = inputArray.slice(startIndex);
+
+    return tailArray;
 
 }
 
--- src/tail.spec.js
+++ src/tail.spec.js
@@ -4,9 +4,17 @@ describe('Tail function',()=>{
 
     it('Should return an empty array if the input array is empty',()=>{
 
-        const inputArray = [];
-        const expectedArray = [];
-        expect(tail(inputArray)).toEqual(expectedArray);
+        const input = [];
+        const expectedOutput = [];
+        expect(tail(input)).toEqual(expectedOutput);
+
+    });
+
+    it('Should return everything except first element',()=>{
+
+        const numbers = [1,2,3,4];
+        const expectedNumbers = [2,3,4];
+        expect(tail(numbers)).toEqual(expectedNumbers);
 
     });
 
